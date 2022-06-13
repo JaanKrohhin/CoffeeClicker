ec328107c802275d6e099fa336d02c1ab90533e5
Calm <Calm@DESKTOP-NIJN9PO>
2022-06-13 11:53:03 +0300

    Add project files.

---
+++ Clicker/ClickerScreen.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Clicker
+{
+    public partial class ClickerScreen : ContentPage
+    {
+        static int points = 0;
+        static int multiplier= 0;
+        public ClickerScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImageButton_Pressed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
